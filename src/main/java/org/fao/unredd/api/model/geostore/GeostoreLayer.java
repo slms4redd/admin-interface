6a8e770b0ddce93700f348f2d5c94d5f001948d7
Fernando Gonzalez Cortes <fernando.gonzalez@geomati.co>
2013-01-18 12:46:02 +0100

    API working with demo1

--- src/main/java/org/fao/unredd/api/model/geostore/GeostoreLayer.java
+++ src/main/java/org/fao/unredd/api/model/geostore/GeostoreLayer.java
@@ -1,9 +1,13 @@
 package org.fao.unredd.api.model.geostore;
 
+import it.geosolutions.geostore.core.model.Attribute;
 import it.geosolutions.geostore.core.model.Resource;
+
+import java.util.List;
 
 import org.fao.unredd.api.json.LayerRepresentation;
 import org.fao.unredd.api.model.Layer;
+import org.fao.unredd.api.model.LayerType;
 
 public class GeostoreLayer implements Layer {
 
@@ -15,8 +19,14 @@ public class GeostoreLayer implements Layer {
 
 	@Override
 	public LayerRepresentation getJSON() {
-		LayerRepresentation ret = new LayerRepresentation(
-				Long.toString(resource.getId()), resource.getName(), null);
-		throw new UnsupportedOperationException("Fix the null");
+		List<Attribute> attributes = resource.getAttribute();
+		LayerType layerType = null;
+		for (Attribute attribute : attributes) {
+			if ("LayerType".equals(attribute.getName())) {
+				layerType = LayerType.valueOf(attribute.getValue());
+			}
+		}
+		return new LayerRepresentation(Long.toString(resource.getId()),
+				resource.getName(), layerType);
 	}
 }
--- src/main/java/org/fao/unredd/api/resources/LayerListResource.java
+++ src/main/java/org/fao/unredd/api/resources/LayerListResource.java
@@ -22,11 +22,14 @@ import org.fao.unredd.api.json.LayerRepresentation;
 import org.fao.unredd.api.json.ResponseRoot;
 import org.fao.unredd.api.model.Layer;
 import org.fao.unredd.api.model.Layers;
+import org.fao.unredd.api.model.geostore.GeostoreLayers;
 
 @Path("/layers")
 public class LayerListResource {
 
-	public static Layers layers;
+	public static Layers layers = new GeostoreLayers(
+			"http://demo1.geo-solutions.it/stg_geostore/rest", "admin",
+			"Unr3dd");
 
 	@Context
 	private UriInfo uriInfo;
