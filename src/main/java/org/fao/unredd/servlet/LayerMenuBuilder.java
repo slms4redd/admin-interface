55f0a8261e8f72a23f979bdae8c02ea149eeba9c
Damianofds <damianofds@gmail.com>
2016-02-04 06:06:39 +0100

    first commit of layers.json working GUI

--- src/main/java/org/fao/unredd/servlet/LayerMenuBuilder.java
+++ src/main/java/org/fao/unredd/servlet/LayerMenuBuilder.java
@@ -1,11 +1,25 @@
 package org.fao.unredd.servlet;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URISyntaxException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 
 /**
  * 
@@ -19,11 +33,100 @@ public class LayerMenuBuilder extends AdminGUIAbstractServlet {
     @Override
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String outputPage = getServletConfig().getInitParameter("outputPage");
+        String operation = request.getParameter("op");
+        if("LOAD".equalsIgnoreCase(operation)){
+            response.setContentType("application/json");      
+            OutputStream os = null;
+            InputStream is = null;
+            try {
+                os = response.getOutputStream();
+                PropertiesLoader pl = new PropertiesLoader();
+                is = pl.getLayersJsonInputStream();
+                IOUtils.copy(is, os);
+                os.flush();
+            } catch (IOException e) {
+                if(is != null){
+                    try {
+                        is.close();
+                    } catch (IOException e1) {
+                        is = null;
+                    }
+                }
+            }
+            finally{
+                if(is != null){
+                    is.close();
+                }
+            }
+        }else{
+            String outputPage = getServletConfig().getInitParameter("outputPage");
+            RequestDispatcher rd = request.getRequestDispatcher(outputPage);
+            rd.forward(request, response);
+        }
         
-        RequestDispatcher rd = request.getRequestDispatcher(outputPage);
-        rd.forward(request, response);
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String layersjson = IOUtils.toString(request.getReader());
+        try {
+            Gson gson = new Gson();
+            gson.fromJson(layersjson, Object.class); 
+        } catch(JsonSyntaxException e) { 
+            throw new IOException("Error parsing JSON request string");
+        }
+        PropertiesLoader pl = new PropertiesLoader();
+        File f = pl.getLayersJson();
+        String layersjson_path = f.getAbsolutePath();
+        File fout = new File(f.getAbsolutePath() + "bkp");
+        fout.createNewFile();
+        FileUtils.copyFile(f, fout);
+        try{
+            FileUtils.forceDelete(f);
+            f = new File(layersjson_path);
+            f.createNewFile();
+            FileUtils.writeStringToFile(f, layersjson);
+        }catch(Exception e){
+            // try at least to recover the backup
+            f = new File(layersjson_path);
+            f.createNewFile();
+            FileUtils.copyFile(fout, f);
+        }
+    }
+    
+    class PropertiesLoader{
         
+        public final String LAYER_JSON_PATH = "path";
+        Configuration prop;
+        
+        public PropertiesLoader() throws IOException{
+            try {
+                prop = new PropertiesConfiguration("layer_json_path.properties");
+            } catch (ConfigurationException e) {
+                throw new IOException("Unable to load properties file");
+            }
+        }
+        
+        public InputStream getLayersJsonInputStream() throws IOException{
+            return new FileInputStream(getLayersJson());
+        }
+        
+        public File getLayersJson() throws IOException{
+            String path = (String)prop.getProperty(LAYER_JSON_PATH);
+            if(path == null || path.isEmpty()){
+                try {
+                    return new File(LayerMenuBuilder.class.getResource("layers_example.json").toURI());
+                } catch (URISyntaxException e) {
+                    throw new IOException("the layers.json path is wrong");
+                }
+            }
+            File f = new File(path);
+            if(f==null || !f.exists() || f.isDirectory() || !f.canWrite()){
+                throw new IOException("the layers.json path is wrong");
+            }
+            return f;
+        }
     }
 
 }
--- src/main/webapp/menulayer-builder.jsp
+++ src/main/webapp/menulayer-builder.jsp
@@ -2,8 +2,69 @@
 <%@taglib prefix="t" tagdir="/WEB-INF/tags"%>
 <%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 
+<script src="http://rawgit.com/jdorn/json-editor/master/dist/jsoneditor.min.js"></script>
+
 <c:set var="bodyContent">
-	<h1>menulayer UI - work in progress...</h1>
+	<div id='title' style="width:800px; margin:0 auto;"><h2>Create the menu for the portal layers</h2></div>
+    
+    <div id='editor_holder' style="width:800px; margin:0 auto;"></div>
+    <button id="submit">Submit (console.log)</button>
+    
+    <script>
+      var schema = "";
+      //retrieve the schema
+      $.ajax({
+		  dataType: "json",
+		  url: "layers-menu-schemas/layers-menu-schema.json",
+		  error: function(){
+		  		alert('an error occurred while loading the schema file');
+  			},
+		  success: function(data, textStatus, jqXHR){
+			  //retrieve the existing layers.json
+			  schema=data;
+		      $.ajax({
+				  dataType: "json",
+				  url: "LayerMenuBuilder?op=load",
+				  success: buildEditor,
+				  error: function(){ 
+					  window.alert('an error occurred while loading the current json file');
+				  }
+			  });
+		  }
+	  });
+      
+      function buildEditor(data, textStatus, jqXHR){
+	      JSONEditor.defaults.options.theme = 'bootstrap3';
+	      var editor = new JSONEditor(document.getElementById('editor_holder'),
+	    	{
+			    theme : 'bootstrap3',
+			    iconlib: 'bootstrap3',
+			    disable_properties: 'true',
+			    disable_collapse: 'true',
+			    disable_edit_json: 'true',
+			    schema: schema,
+			    startval: data
+	    	});
+	      
+	      
+	      document.getElementById('submit').addEventListener('click',function() {
+	    	  $.ajax({
+	    		  method: "POST",
+	    		  data: JSON.stringify(editor.getValue()),
+				  url: "LayerMenuBuilder",
+				  success: function(){
+					  window.alert('layer-menu json file successfully updated!');
+				  },
+				  error: function(){
+					  window.alert('an error occurred while storing the json file');
+				  }
+			  });
+	          console.log(editor.getValue());
+	      });
+      }
+    </script>
+	
+	
 </c:set>
 
 <t:mainlayout>
